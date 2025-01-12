c2bdf197e602e25105350055f95343fe54a00819
duskcus <erwin.yuen@live.nl>
2025-01-12 13:20:43 +0100

    Added anchor preset and disabled tool

--- addons/godot_dynamic/scripts/dynamic_menu.gd
+++ addons/godot_dynamic/scripts/dynamic_menu.gd
@@ -1,9 +1,7 @@
-@tool
 extends Control
 
-#region vars
-@export var navbar_items : Array
-@export var menu_items : Array
+@export var navbar_links : PackedStringArray
+@export var menu_links : Array
 @export var navbar : bool = true
 @export var header : bool = true
 @export var menu : bool = true
@@ -11,26 +9,56 @@ extends Control
 var vbox = VBoxContainer
 var hbox = HBoxContainer
 var button : Button
-var value = 0
+var path
 var scene : PackedScene = preload("res://addons/godot_dynamic/scenes/dynamic_menu/dynamic_menu.tscn")
-#endregion
+
+	#var path = str(menu1.get_path()) + "/" + menu_items[active_item]
+	#var target_node = get_node(path)
+	#target_node.add_theme_color_override("font_color", Color(1, 0, 0))
 
 func _enter_tree():
 	# Instance the scene
 	add_child(scene.instantiate())
 	
-	if navbar_items:
-		for item in navbar_items:
+	if navbar_links:
+		var value = 0
+		for item in navbar_links:
+			# Create buttons
 			button = Button.new()
-			button.text = navbar_items[value]
+			#button.name = str(value)
+			button.name = str(item)
+			button.text = item
 			$DynamicMenu/Panel/HBoxContainer.add_child(button)
+			
+			# Create vboxs
+			vbox = VBoxContainer.new()
+			vbox.name = str(value)
+			vbox.layout_mode = 1
+			vbox.set_anchors_preset(Control.PRESET_HCENTER_WIDE)
+			$DynamicMenu.add_child(vbox)
+			
+			# Makes everything but the first nav item invisible
+			if value != 0:
+				path = "DynamicMenu/" + str(value) + "/"
+				get_node(path).visible = false
+			
 			value += 1
 	
-	value = 0
 	
-	if menu_items:
-		for item in menu_items:
-			button = Button.new()
-			button.text = menu_items[value]
-			$DynamicMenu/VBoxContainer.add_child(button)
+	if menu_links:
+		var value = 0
+		for item in menu_links:
+			path = "DynamicMenu/" + str(value) + "/"
+				
+			if item is PackedStringArray and item != null:
+				for menu_items in item:
+					button = Button.new()
+					button.name = menu_items
+					button.text = menu_items
+					get_node(path).add_child(button)
+			else:
+				button = Button.new()
+				button.text = str(item)  # Added str() conversion for non-string items
+				get_node(path).add_child(button)
+	
 			value += 1
